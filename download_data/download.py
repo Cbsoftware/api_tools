3ed2d85befda4c77e91b04789eb090476e93d527
NatalieBlack <natalievictoriablack@gmail.com>
2015-02-16 17:02:24 -0500

    fixes

--- download_data/download.py
+++ download_data/download.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python
 
 import sys
+import os
+import json
 import requests
 import arrow, datetime, time
 from config import apikey, startsecond, startminute, starthour, startday, startmonth, startyear, endsecond, endminute, endhour, endday, endmonth, endyear, sleepamount
 from dateutil.tz import tzutc 
 
+jobname = sys.argv[1]
 service = 'api/pressure'
 
-#not currently using
-def save_data(data, jobname, starttime):
+def make_dirs(jobname):
+    dname = 'data'
+    if not os.path.exists(dname):
+        os.makedirs(dname)
+
+    #downloaded data will be stored in this directory
+    if not os.path.exists(os.path.join(dname, jobname)):
+        os.makedirs(os.path.join(dname, jobname))
+
+def save_data(data, jobname, stime):
     dname = 'data'
     make_dirs(jobname)
 
     fn = os.path.join(dname, jobname, '{starttime}.json'.format(
-            starttime = origstart.format('MMMM-DD-YYYY:HH:mm:ss'),
+            starttime = stime.format('MMMM-DD-YYYY:HH:mm:ss'),
             ))
 
     print "Data saved to " + fn
@@ -29,14 +40,14 @@ def success(r, data):
 
     return data
 
-def make_call(params, data, t):
+def make_call(params, data, t, jobname):
     r = requests.get('https://pressurenet.io/' + service, params=params)
     print "Request made to " + r.url
     print arrow.get(str(params['timestamp']/1000)).format('MMMM-DD-YYYY:HH:mm:ss')
     print "Status: {}".format(r.status_code)
     if r.status_code == 200:
         data = success(r, data)
-        save_data(data, jobname, t)
+        save_data(data, jobname, arrow.get(t))
 
     return r
 
@@ -50,7 +61,7 @@ t = stime.timestamp * 1000
 data = []
 while(t < endtime):
     params = {'timestamp': t, 'api_key':apikey, 'format':'json'}
-    make_call(params, data, t/1000)
+    make_call(params, data, t/1000, jobname)
     t = arrow.get(t / 1000).replace(seconds=+600).timestamp * 1000
 
 print "finished"
