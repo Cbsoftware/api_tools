1724abe4de89c4a467662c5789ec504b61b4bbed
NatalieBlack <natalievictoriablack@gmail.com>
2015-02-17 11:22:59 -0500

    revert to api v1 script

--- download_data/download.py
+++ download_data/download.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python
 
-import sys
 import os
-import json
+import sys
 import requests
+import json
 import arrow, datetime, time
-from config import apikey, startsecond, startminute, starthour, startday, startmonth, startyear, endsecond, endminute, endhour, endday, endmonth, endyear, sleepamount
 from dateutil.tz import tzutc 
+from config import apikey, startsecond, startminute, starthour, startday, startmonth, startyear, endsecond, endminute, endhour, endday, endmonth, endyear, minlat, maxlat, minlon, maxlon, sleepamount, mins1, mins2
 
-jobname = sys.argv[1]
-service = 'api/pressure'
+
+#which API to call
+if sys.argv[1] == 'conditions': 
+    service = 'conditions/list'
+elif sys.argv[1] == 'readings':
+    service = 'live'
+else:
+    print 'Unknown service'
+
+def get_dname():
+    if service == 'conditions/list':
+        dname = 'conditions'
+    else:
+        dname = 'data'
+       
+    return dname
 
 def make_dirs(jobname):
-    dname = 'data'
+    dname = get_dname()
     if not os.path.exists(dname):
         os.makedirs(dname)
 
@@ -20,12 +34,15 @@ def make_dirs(jobname):
     if not os.path.exists(os.path.join(dname, jobname)):
         os.makedirs(os.path.join(dname, jobname))
 
-def save_data(data, jobname, stime):
-    dname = 'data'
+def save_data(data, jobname):
+    dname = get_dname()
     make_dirs(jobname)
 
-    fn = os.path.join(dname, jobname, '{starttime}.json'.format(
-            starttime = stime.format('MMMM-DD-YYYY:HH:mm:ss'),
+    fn = os.path.join(dname, jobname, '{startdate}_{enddate}_{minlat}-{maxlat}_{minlon}-{maxlon}.json'.format(
+            startdate = origstart.format('MMMM-DD-YYYY:HH:mm:ss'),
+             enddate = ftime.format('MMMM-DD-YYYY:HH:mm:ss'),
+             minlat = minlat, maxlat = maxlat, 
+             minlon = minlon, maxlon = maxlon
             ))
 
     print "Data saved to " + fn
@@ -39,31 +56,111 @@ def success(r, data):
 
     if len(r.json()) > 0:
         data += r.json()
-  
-    return data
 
-def make_call(params, data, t, jobname):
-    r = requests.get('https://pressurenet.io/' + service, params=params)
-    print "Request made to " + r.url
-    print arrow.get(str(params['timestamp']/1000)).format('MMMM-DD-YYYY:HH:mm:ss')
-    print "Status: {}".format(r.status_code)
-    if r.status_code == 200:
-        data = success(r, data)
+def attempt(params, data, jobname):
+    s = params['start_time']
+    f = params['end_time']
 
-    return r
+    try:
+        r = requests.get('https://pressurenet.io/' + service, params=params)
+        print "Request made to " + r.url
+        print arrow.get(str(s/1000)).format('MMMM-DD-YYYY:HH:mm:ss') + " to " + arrow.get(str(f/1000)).format('MMMM-DD-YYYY:HH:mm:ss')
+        print "Status: {}".format(r.status_code)
+        if r.status_code == 200:
+            success(r, data)
+
+        return r
+    except:
+        print "An error occured"
+        print sys.exc_info()[0] 
+        save_data(data, jobname)
+
+def log(r, s, f, jobname):
+    make_dirs(jobname)
+
+    errorlog = open('data/' + jobname + '/error_log', 'a+')
+    errorlog.write(str(r.status_code) + "\n")
+    errorlog.write(r.url + "\n")
+    errorlog.write(str(arrow.get(s / 1000).date()) + " " + str(arrow.get(s / 1000).time()) + "\n")
+    errorlog.write(str(arrow.get(f / 1000).date()) + " " + str(arrow.get(f / 1000).time()) + "\n")
+    return errorlog
+
+def make_call(s, f, data, jobname):
+    logfile = None
+    params = {'api_key': apikey, 'start_time': s, 'end_time': f,
+               'min_lat': minlat, 'max_lat': maxlat,
+               'min_lon': minlon, 'max_lon': maxlon 
+              }
+    
+    r = attempt(params, data, jobname)
+    
+    if r.status_code != 200:
+        time.sleep(2*sleepamount)
+        print "Retrying"
+        r2 = attempt(params, data, jobname)
+        time.sleep(30 + sleepamount)
+        if r2.status_code != 200:
+            logfile = log(r2, s, f, jobname)
+
+    if logfile:
+        logfile.close()
+
+
+if maxlat <=minlat:
+    print "The maximum latitude must be greater than the minimum latitude."
+    exit()
+if maxlon <=minlon:
+    print "The maximum longitude must be greater than the minimum longitude."
+    exit()
+
+try:
+    jobname = sys.argv[2]
+except:
+    jobname = str(arrow.get())
+
+
 
 stime = arrow.get(datetime.datetime(startyear, startmonth, startday, starthour, startminute, startsecond, tzinfo=tzutc()))
+origstart = stime
 
-etime = arrow.get(datetime.datetime(endyear, endmonth, endday, endhour, endminute, endsecond, tzinfo=tzutc()))
-
-endtime = etime.timestamp * 1000
-t = stime.timestamp * 1000
+ftime = arrow.get(datetime.datetime(endyear, endmonth, endday, endhour, endminute, endsecond, tzinfo=tzutc()))
+origfinish = ftime.timestamp * 1000
 
 data = []
-while(t < endtime):
-    params = {'timestamp': t, 'api_key':apikey, 'format':'json'}
-    make_call(params, data, t/1000, jobname)
-    t = arrow.get(t / 1000).replace(seconds=+600).timestamp * 1000
 
-save_data(data, jobname, arrow.get(t/1000))
-print "finished"
+ftimestamp = ftime.timestamp * 1000
+
+#don't ask for more than 1 day's data at a time
+while ((((ftime - stime).days >= 1) or ((ftime - stime).seconds > (60*mins1))) and (ftimestamp <= origfinish)):
+
+    stimestamp = stime.timestamp * 1000
+    ftimestamp = stime.replace(seconds=+(60*mins1)).timestamp * 1000
+
+    make_call(stimestamp, ftimestamp, data, jobname)
+
+    stime = arrow.get(ftimestamp / 1000)
+    time.sleep(sleepamount)
+
+    stimestamp = stime.timestamp * 1000
+    ftimestamp = stime.replace(seconds=+(60*mins1)).timestamp * 1000
+
+    make_call(stimestamp, ftimestamp, data, jobname)
+
+    stime = arrow.get(ftimestamp / 1000)
+    time.sleep(sleepamount)
+
+while ((ftime - stime).seconds > (60*mins2) and (ftimestamp <= origfinish)):
+    stimestamp = stime.timestamp * 1000
+    ftimestamp = stime.replace(seconds=+(60*mins2)).timestamp * 1000
+
+    make_call(stimestamp, ftimestamp, data, jobname)
+
+    stime = arrow.get(ftimestamp / 1000)
+    time.sleep(sleepamount)
+
+stimestamp = stime.timestamp * 1000
+ftimestamp = ftime.timestamp * 1000
+
+make_call(stimestamp, ftimestamp, data, jobname)
+
+save_data(data, jobname)
