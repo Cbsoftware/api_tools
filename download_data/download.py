2fc4ac177acb6e4ee8281990242cb5e9780cbba1
NatalieBlack <natalievictoriablack@gmail.com>
2015-03-10 18:14:51 -0400

    can now be used as module or script

--- download_data/download.py
+++ download_data/download.py
@@ -8,7 +8,6 @@ import arrow, datetime, time
 from config import apikey, startsecond, startminute, starthour, startday, startmonth, startyear, endsecond, endminute, endhour, endday, endmonth, endyear, sleepamount, dataformat, minlat, maxlat, minlon, maxlon
 from dateutil.tz import tzutc 
 
-jobname = sys.argv[1]
 service = 'api/pressure'
 
 FILTERING = minlat > -90 or maxlat < 90 or minlon > -180 or maxlon < 180
@@ -73,27 +72,30 @@ def make_call(params, data, t, jobname):
     print "Status: {}".format(r.status_code)
     if r.status_code == 200:
         data = success(r, data)
-        print len(data)
         if FILTERING:
             data = geo_filter(data)
-        print len(data)
     else:
         print "an error occurred: " + str(r.status_code)
 
     return data
 
-stime = arrow.get(datetime.datetime(startyear, startmonth, startday, starthour, startminute, startsecond, tzinfo=tzutc()))
+def download_stuff(jobname):
+    stime = arrow.get(datetime.datetime(startyear, startmonth, startday, starthour, startminute, startsecond, tzinfo=tzutc()))
 
-etime = arrow.get(datetime.datetime(endyear, endmonth, endday, endhour, endminute, endsecond, tzinfo=tzutc()))
+    etime = arrow.get(datetime.datetime(endyear, endmonth, endday, endhour, endminute, endsecond, tzinfo=tzutc()))
 
-endtime = etime.timestamp * 1000
-t = stime.timestamp * 1000
+    endtime = etime.timestamp * 1000
+    t = stime.timestamp * 1000
 
-data = []
-while(t < endtime):
-    params = {'timestamp': t, 'api_key':apikey, 'format':dataformat}
-    data = make_call(params, data, t/1000, jobname)
-    t = arrow.get(t / 1000).replace(seconds=+600).timestamp * 1000
+    data = []
+    while(t < endtime):
+        params = {'timestamp': t, 'api_key':apikey, 'format':dataformat}
+        data = make_call(params, data, t/1000, jobname)
+        t = arrow.get(t / 1000).replace(seconds=+600).timestamp * 1000
 
-save_data(data, jobname, stime, etime, dataformat)
-print "finished"
+    save_data(data, jobname, stime, etime, dataformat)
+    print "finished"
+
+if __name__ == "__main__":
+    jobname = sys.argv[1]
+    download_stuff(jobname)
